ea7a166835cc21a2ece188ec2e21dcbe2a30f6c7
Igor Zubkov <igor.zubkov@gmail.com>
2011-12-20 13:25:56 +0200

    Init

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_landing_page do
+  name 'Redmine Landing Page plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
