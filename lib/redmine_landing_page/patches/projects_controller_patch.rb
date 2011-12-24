dfcd531916b38a2605411d6022aa2d0ed187202d
Igor Zubkov <igor.zubkov@gmail.com>
2011-12-24 03:48:06 +0200

    Fix projects controller patch

--- lib/redmine_landing_page/patches/projects_controller_patch.rb
+++ lib/redmine_landing_page/patches/projects_controller_patch.rb
@@ -8,7 +8,7 @@ module RedmineLandingPage
           alias_method :show_without_landing_page, :show unless method_defined? :show_without_landing_page
 
           def show
-            if !@project.landing_page.empty?
+            if @project.landing_page && !@project.landing_page.empty?
               redirect_to @project.landing_page, :status => 302
             else
               show_without_landing_page
