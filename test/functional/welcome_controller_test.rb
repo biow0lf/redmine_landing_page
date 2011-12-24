357d626381d9fadc1c8d0599f1d7bb303538a53e
Igor Zubkov <igor.zubkov@gmail.com>
2011-12-24 15:58:51 +0200

    Add functional tests for welcome controller patch

---
+++ test/functional/welcome_controller_test.rb
@@ -0,0 +1,22 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require_dependency 'welcome_controller'
+
+class WelcomeControllerTest < ActionController::TestCase
+  def setup
+    @request = ActionController::TestRequest.new
+    @response = ActionController::TestResponse.new
+  end
+
+  def test_show_default_welcome_page
+    get :index
+    assert_response :success
+    assert_template 'index'
+  end
+
+  def test_redirect_to_landing_page
+    @user = User.generate!(:landing_page => "http://#{@request.host}/projects")
+    @request.session[:user_id] = @user.id
+    get :index
+    assert_redirected_to "http://#{@request.host}/projects"
+  end
+end
